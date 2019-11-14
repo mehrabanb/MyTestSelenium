2334cdca6c2410224667ecda200a8ce01a5f9a87
mehraban_b <mehraban_b@Hazama>
2019-11-14 14:36:39 -0500

    First commit

---
+++ SeleniumTest/src/Shopping.java
@@ -0,0 +1,13 @@
+
+
+public class Shopping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver" , "C:\\Users\\bob\\OneDrive\\Selenium\\chromedriver_win32\\chromedriver.exe"); //selenium 
+		WebDriver driver=new ChromeDriver(); 
+
+	}
+
+}
