73e93b551511bec8e2b6141602a6007c9b7a423e
mehraban_b <mehraban_b@Hazama>
2019-11-14 14:39:59 -0500

    update

--- SeleniumTest/src/Shopping.java
+++ SeleniumTest/src/Shopping.java
@@ -1,4 +1,5 @@
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Shopping {
 
@@ -7,7 +8,9 @@ public class Shopping {
 		
 		System.setProperty("webdriver.chrome.driver" , "C:\\Users\\bob\\OneDrive\\Selenium\\chromedriver_win32\\chromedriver.exe"); //selenium 
 		WebDriver driver=new ChromeDriver(); 
-
+		
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize(); 
 	}
 
 }
